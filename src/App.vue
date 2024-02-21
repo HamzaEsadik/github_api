3dc4eee586e22ff5ae2ac0aa40b5e3116ef9f172
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-02-21 13:45:42 +0100

    basic setup

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,20 @@
+<template>
+    <base-card>{{ name }}</base-card>
+</template>
 
+<script>
+    export default {
+        data() {
+            return {
+                name: 'hamza',
+                id: 56849
+            }
+        },
+        methods: {
+            setName() {
+                this.name = esadik;
+            }
+        }
+    }
+</script>
+<style></style>
---
+++ src/components/UI/BaseCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+div {
+    height: 100px;
+    width: 80%;
+    margin: auto;
+}
+</style>
